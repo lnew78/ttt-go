a32ee95f61562fb1cd935f1637635a953a570a89
mnewsome <malcolm.newsome@gmail.com>
2014-03-31 15:19:26 -0500

    adds new methods to board struct

--- ttt/board.go
+++ ttt/board.go
@@ -16,17 +16,27 @@ func NewBoard(size int) *Board {
   return board
 }
 
-func ( board *Board ) Spaces() []string {
-  return board.spaces
-}
-
-func ( board *Board) FillSpaceAt(space int, mark string) {
-  board.spaces[space - 1] = mark
-}
-
 func (board *Board) InitializeSpaces() {
   board.spaces = make( []string, board.numberOfSpaces)
   for i := range  board.spaces {
     board.spaces[i] = strconv.Itoa(i + 1)
   }
+}
+
+func (board *Board) Spaces() []string {
+  return board.spaces
+}
+
+func (board *Board) FillSpaceAt(space int, mark string) {
+  if board.isSpaceAvailableAt(space) {
+    board.spaces[space - 1] = mark
+  }
+}
+
+func (board *Board) ResetSpaceAt(space int) {
+  board.spaces[space - 1] = strconv.Itoa(space)
+}
+
+func (board *Board) isSpaceAvailableAt(space int) bool{
+  return board.spaces[space - 1] == strconv.Itoa(space)
 }
--- ttt/board_test.go
+++ ttt/board_test.go
@@ -9,16 +9,25 @@ import (
 var _ = Describe("Board", func() {
   var board = NewBoard(9)
 
-    It("Should have numbered spaces starting at 1 and ending at board size", func() {
+    It("Spacs start at 1 and end at board size", func() {
       testArray :=  []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 
-      Expect( board.Spaces()).To(Equal(testArray))
+      Expect(board.Spaces()).To(Equal(testArray))
     })
 
-    It("Space can be filled", func() {
+    It("Spaces can be filled", func() {
       testArray :=  []string{"x", "2", "3", "4", "5", "6", "7", "8", "9"}
       board.FillSpaceAt(1, "x")
 
-      Expect( board.Spaces()).To(Equal(testArray))
+      Expect(board.Spaces()).To(Equal(testArray))
     })
+
+    It("Spaces can be reset to their default value", func() {
+      testArray :=  []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+      board.FillSpaceAt(1, "x")
+      board.ResetSpaceAt(1)
+
+      Expect(board.Spaces()).To(Equal(testArray))
+    })
+
 })
